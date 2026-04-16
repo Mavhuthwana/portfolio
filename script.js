680c70dc94f5a4779024c3dd934a246e350c05bd
25058109 <25058109@mvula.univen.ac.za>
2026-04-16 17:31:55 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+ 
+ function scrollDown(){
+
+document.getElementById("education")
+.scrollIntoView({
+behavior:"smooth"
+});
+
+}
+<script src="https://kit.fontawesome.com/6952b55733.js" crossorigin="anonymous"></script>
